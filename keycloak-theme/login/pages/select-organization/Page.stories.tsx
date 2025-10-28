3799f3053531691236b31546a7925f7c7410137f
garronej <joseph.garrone@protonmail.com>
2025-10-28 11:28:59 +0100

    https://github.com/keycloakify/keycloakify/issues/879

---
+++ keycloak-theme/login/pages/select-organization/Page.stories.tsx
@@ -0,0 +1,14 @@
+import { createKcPageStory, type Meta, type StoryObj } from "../../mocks/KcPageStory";
+
+const { KcPageStory } = createKcPageStory({ pageId: "select-organization.ftl" });
+
+const meta = {
+    title: "login/select-organization.ftl",
+    component: KcPageStory
+} satisfies Meta<typeof KcPageStory>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
